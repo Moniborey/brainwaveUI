f7302162cbbfa53ecaddaa429817956009b97d2c
yuutaaaa <moniboreychay123@gmail.com>
2024-03-06 09:55:13 +0700

    finished

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import Section from './Section'
+import {socials} from '../constants'
+
+export default function Footer() {
+  return (
+    <Section
+    crosses
+    className={'!px-0 !py-10'}
+    >
+    <div className='container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col'>
+        <p className='caption text-n-4'>{new Date().getFullYear()}. All rights reserved.</p>
+        <ul className='flex gap-5 flex-wrap'>
+            {socials.map((icon,index)=>(
+                <a href={icon.url} key={index} target='_blank' className='flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6'>
+                    <img src={icon.iconUrl} width={16} height={16} alt='icon'/>
+                </a>
+            ))}
+        </ul>
+    </div>
+    </Section>
+  )
+}
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,6 +3,7 @@ import Heading from './Heading'
 import { service1, service2, service3, check } from '../assets'
 import { brainwaveServices, brainwaveServicesIcons } from '../constants'
 import Generating from './Generating'
+import {PhotoChatMessage, Gradient, VideoBar ,VideoChatMessage} from './design/Services'
 
 export default function Services() {
     return (
@@ -45,12 +46,42 @@ export default function Services() {
                                     alt='robot'
                                 />
                             </div>
-                            <div className='absolute inset-0 flex flex-col justify-end p-8 '>
-                                <h4 className='h4 mb-4'>Smartest AI</h4>
-                                <p className='body-2 mb-[3rem] text-n-3'>Brainwave unlocks the power of AI-powered applications.</p>
+                            <div className='absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15'>
+                                <h4 className='h4 mb-4'>Photo editing</h4>
+                                <p className='body-2 mb-[3rem] text-n-3'>Automatically enhance your photos using our AI app&#39;s photo editing feature. Try it now!</p>
+                            </div>
+                            <PhotoChatMessage/>
+                        </div>
+                        <div className='p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]'>
+                            <div className='py-12 px-4 xl:px-8'>
+                                <h4 className='h4 mb-4'>Video generation</h4>
+                                <p className='body-2 mb-[2rem] text-n-3'>
+                                    The world&apos;s most powerful AI photo and video art generation engine. What will you create?
+                                </p>
+                                <ul className='flex items-center justify-between'>
+                                    {brainwaveServicesIcons.map((item,index)=>(
+                                        <li className={`flex items-center justify-center rounded-2xl ${index === 2 ? 'w-[3rem] h-[3rem] p-0.5 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]' : 'flex w-10 h-10 bg-n-6 md:w-15 md:h-15'}`} key={index}>
+                                            <div className={`${index===2 && 'flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]'}`}>
+                                                <img width={24} height={24} alt='item' src={item}/>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className='relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]'>
+                                <img
+                                    src={service3}
+                                    className='w-full h-full object-cover'
+                                    width={520}
+                                    height={400}
+                                    alt='scaryrobot'
+                                />
+                                <VideoChatMessage />
+                                <VideoBar />
                             </div>
                         </div>
                     </div>
+                    <Gradient />
                 </div>
             </div>
         </Section>
